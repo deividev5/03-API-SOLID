e88a7342fd4ddaa1441140bfade00ad222c31682
Deivisson Gonçalves <deivissondevcontato@gmail.com>
2026-01-21 12:20:27 -0300

    Carregando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import {app} from './app.js';
+import {env} from './env/index.js'
 
 // Start the server and listen on port 3333
 app.listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log('HTTP server running 🚀');
 })
