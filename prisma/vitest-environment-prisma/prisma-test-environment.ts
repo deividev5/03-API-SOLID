ed5d4629df954073fae2bf420b093c39520df1c2
Deivisson Gonçalves <deivissondevcontato@gmail.com>
2026-03-26 11:33:57 -0300

    Criando test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,53 @@
+import { prisma } from '@/lib/prisma.js'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+
+import { randomUUID } from 'node:crypto'
+import type { Environment } from 'vitest/environments'
+
+// Pega o url no .env que cria a schema no docker
+// e troca o schema no final do URl para a nossa que vamos usar
+function generateDatabaseUrl(schema: string) {
+  // caso não tenha um URL válida, retornamos um erro.
+  if (process.env.DATABASE_URL) {
+    throw new Error(
+      'DATABASE_URL environment variable is already set. Please unset it before running the tests.',
+    )
+  }
+
+  // Trazemos a url que está no nosso .env
+  const url = new URL(process.env.DATABASE_URL!)
+
+  // Definimos um novo valor na parte de schema do nosso url,
+  // passando um novo valor pra ele
+  url.searchParams.set('schema', schema)
+
+  // Retorna a url em forma de string
+  return url.toString()
+}
+
+export default <Environment>{
+  // configuração da ciração do banco
+  name: 'prisma',
+  transformMode: 'ssr',
+  // Vamos criar um novo banco.
+  async setup() {
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma db push')
+
+    // Quando terminar o teste deletamos o banco
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
---
+++ vite.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+
+  // Criando configurações específicas para cada tipo de testes
+  test: {
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-encironmet.ts',
+        },
+      },
+    ],
+  },
+})
