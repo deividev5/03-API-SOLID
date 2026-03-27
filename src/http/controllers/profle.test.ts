8d9497aacda5b23487ba5af112f716494fe0e6e9
Deivisson Gonçalves <deivissondevcontato@gmail.com>
2026-03-27 11:33:26 -0300

    Teste E2E do perfil

--- src/http/controllers/profle.test.ts
+++ src/http/controllers/profle.test.ts
@@ -21,16 +21,27 @@ describe('Autenticação (e2e)', () => {
       password: '123456',
     })
 
-    // Eviando um os dado para a rota post, para fazer a autenticação do usuário
-    const response = await request(app.server).post('/sessions').send({
+    // Eviando o dados de um usuário existente para ter o token de autenticação
+    const responseAuth = await request(app.server).post('/sessions').send({
       email: 'johndoe@example.com',
       password: '123456',
     })
 
-    // Esperamos que o staus code da nossa solicitação seja de 201(deu certo)
+    // Pegando o token criado da autenticação
+    const { token } = responseAuth.body
+
+    // Vamos testar a rota de profile do usuário passando o token junto par ela ser valida
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    // Esperamos que retorne status code 200 e no objeto user do corpo resposta venha o email
     expect(response.statusCode).toEqual(200)
-    expect(response.body).toEqual({
-      token: expect.any(String),
-    })
+    expect(response.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
   })
 })
